02c0a809f028ef04613c6336b5c00f390710c45e
TarasMysiura <tarasmysiura@gmail.com>
2023-08-01 16:30:16 +0300

    add

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { RegistrationScreen } from './components/Screens/RegistrationScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Привіт всім</Text>
+      <RegistrationScreen />
       <StatusBar style="auto" />
     </View>
   );
